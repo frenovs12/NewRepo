aab5d5f5b5b2842ee735e72c00a430f7a1615eac
Ons MOKNI <moknions@gmail.com>
2019-11-27 23:03:16 +0100

    send_messages

--- PIDEV.Presentation/Controllers/HomeController.cs
+++ PIDEV.Presentation/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +12,18 @@ namespace PIDEV.Presentation.Controllers
     {
         public ActionResult Index()
         {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(" test project", "ons.mokni@esprit.tn"));
+            message.To.Add(new MailboxAddress(" ons", "moknions@gmail.com"));
+            message.Subject = "u got a +";
+            message.Body = new TextPart("plain") { Text = "u got & plus "};
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, false);
+                client.Authenticate("ons.mokni@esprit.tn", "ibrahimamel");
+                client.Send(message);
+                client.Disconnect(true);
+            }
             return View();
         }
 
